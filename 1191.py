3e09cc60a73e49c9bf7788c8f283a378c9095739
muslimbek77 <muslimjonbekali@mail.ru>
2025-04-10 21:21:41 +0500

    1191 update

--- 1191.py
+++ 1191.py
@@ -1,16 +1,4 @@
 n, m, k, d = map(int, input().split())
-
 time_bus = d + n * m
 time_walk = n * k
-min_time = min(time_bus, time_walk)
-
-if k > m:
-    denominator = k - m
-    i_min = (d + denominator - 1) // denominator
-    if i_min <= n:
-        for i in range(i_min, n + 1):
-            current_time = i * k + (n - i) * m
-            if current_time < min_time:
-                min_time = current_time
-
-print(min_time)
+print(min(time_bus, time_walk))
