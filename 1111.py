43cdffe8cf7d0f54472b939ed7dc4063578bab4b
muslimbek77 <muslimjonbekali@mail.ru>
2025-04-15 12:40:26 +0500

    1111 update

--- 1111.py
+++ 1111.py
@@ -1,12 +1,19 @@
+import math
+
 def umumiy_boluvchilar_soni(n, m):
-    # Eng kichikini topamiz
-    kichik = min(n, m)
-    # Hisoblagich
+    # n va m ning eng katta umumiy bo'luvchisini topamiz
+    g = math.gcd(n, m)
     count = 0
-    # 1 dan kichik songacha bo'lgan barcha sonlarni tekshiramiz
-    for i in range(1, kichik + 1):
-        if n % i == 0 and m % i == 0:
-            count += 1
+    # g ning ildizigacha bo'lgan sonlarni tekshiramiz
+    for i in range(1, int(math.sqrt(g)) + 1):
+        if g % i == 0:
+            # Agar i ning kvadrati g ga teng bo'lsa, ikkita o'rniga bitta bo'luvchi hisoblanadi
+            if i * i == g:
+                count += 1
+            else:
+                count += 2  # i va g//i ikki xil bo'luvchi
     return count
+
+# Input olib, natijani chiqaramiz
 a, b = map(int, input().split())
 print(umumiy_boluvchilar_soni(a, b))
