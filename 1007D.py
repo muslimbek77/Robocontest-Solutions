39c0ca11bb09e1a926924a2e776df6ae17c06d09
muslimbek77 <muslimbekbaratov57@gmail.com>
2025-09-23 08:05:44 +0500

    1007D update

--- 1007D.py
+++ 1007D.py
@@ -1,14 +1,9 @@
-N= int(input())
-result = [1]
-j0 = 0
-j1 = 1
+N,A,D = map(int,input().split())
+result = []
+j = A
 for i in range(N):
-    j2 = j1
-    j1 = j1 + j0
-    j0 = j2
-
-    result.append(j1)
-    
+    result.append(j)
+    j *= D
 
 for i in result:
     print(i,end=' ')
